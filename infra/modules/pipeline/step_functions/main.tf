c5f22c216db8897c65c0a5cbbef4875cebb5fd50
Benjamin Cassatt <benjamin.cassatt@gmail.com>
2026-03-01 17:17:48 -0500

    deleting files

--- infra/modules/pipeline/step_functions/main.tf
+++ infra/modules/pipeline/step_functions/main.tf
@@ -33,9 +33,12 @@ resource "aws_iam_role_policy" "sfn_invoke_lambdas" {
   })
 }
 
-data "template_file" "asl" {
-  template = file("${path.module}/state_machine.asl.json")
-  vars = {
+resource "aws_sfn_state_machine" "pipeline" {
+  name     = "${var.project_name}-${var.environment}-pipeline"
+  role_arn = aws_iam_role.sfn_role.arn
+  type     = "STANDARD"
+
+  definition = templatefile("${path.module}/state_machine.asl.json", {
     lambda_classify_arn       = var.lambda_arns.classify
     lambda_start_textract_arn = var.lambda_arns.start_textract
     lambda_check_textract_arn = var.lambda_arns.check_textract
@@ -43,14 +46,7 @@ data "template_file" "asl" {
     lambda_normalize_arn      = var.lambda_arns.normalize
     lambda_llm_extract_arn    = var.lambda_arns.llm_extract
     lambda_persist_arn        = var.lambda_arns.persist
-  }
-}
-
-resource "aws_sfn_state_machine" "pipeline" {
-  name       = "${var.project_name}-${var.environment}-pipeline"
-  role_arn   = aws_iam_role.sfn_role.arn
-  definition = data.template_file.asl.rendered
-  type       = "STANDARD"
+  })
 }
 
 # Store SFN ARN in SSM so starter lambda can read it without terraform cycles
